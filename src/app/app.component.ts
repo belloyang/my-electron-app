361faec664a84993c735f814ba7f7618a01bc09e
Bing Yang <bingyang@nanometrics.ca>
2018-10-05 12:23:54 -0400

    use BehaviorSubject and catch enter keydown event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import {spawn, ChildProcess} from "child_process";
-
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 
 
@@ -17,13 +17,17 @@ export class AppComponent implements OnInit {
   stdin:string="";
   outputLog:string = "";
   inputLog:string = "";
-  activeChild:ChildProcess = null;
+  activeChild:ChildProcess|any = null;
   activeCmd = "";
 
+  outputLog$:BehaviorSubject<string>;
+  inputLog$:BehaviorSubject<string>;
 
 
   constructor(){
     console.log("AppComponent constructed!");
+    this.outputLog$ = new BehaviorSubject<string>(this.outputLog);
+    this.inputLog$ = new BehaviorSubject<string>(this.inputLog);
   }
 
   ngOnInit(): void {
@@ -33,18 +37,21 @@ export class AppComponent implements OnInit {
   }
 
   processInput(stdin:string){
+    let self = this;
     if(this.activeChild == null){
       console.log("Executing command:"+stdin);
       this.activeCmd = stdin;
       this.activeChild = spawn(stdin).on('error', 
       function( err:any ){ 
         console.log(err); 
+        self.activeChild = null;
       });
 
-      let self = this;
+      
       this.activeChild.stdout.on('data', (data:any) => {
         console.log(`child stdout:\n${data}`);
         this.outputLog += data.toString();
+        this.outputLog$.next(this.outputLog);
       });
     
       
@@ -52,6 +59,7 @@ export class AppComponent implements OnInit {
         this.activeChild.stderr.on('data', (data:any) => {
           console.error(`child stderr:\n${data}`);
           this.outputLog += data.toString();
+          this.outputLog$.next(this.outputLog);
         });
   
         this.activeChild.on('exit', function (code:any, signal:any) {
@@ -64,6 +72,7 @@ export class AppComponent implements OnInit {
 
      console.log("stream input to child stdin",stdin, stdin.length);
       this.inputLog += stdin +'\n';
+      this.inputLog$.next(this.inputLog);
       this.activeChild.stdin.write(stdin+'\n',()=>{
         console.log(stdin +" is written into activechild.stdin");
       });
